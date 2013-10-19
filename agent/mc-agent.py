e693876c850d40a8b364b1bf1bba0cd6a6bf268c
mzasada <maciejekz@gmail.com>
2013-10-19 19:07:27 +0200

    Changed agent scheduling to cron based.

--- agent/mc-agent.py
+++ agent/mc-agent.py
@@ -1,7 +1,8 @@
 import pymongo
+import atexit
 from apscheduler.scheduler import Scheduler
 
-scheduler = Scheduler()
+scheduler = Scheduler(daemon=True)
 db = pymongo.MongoClient('mongodb://localhost:27017/')['ecp']
 
 
@@ -13,12 +14,13 @@ def send_collection_stats(collection):
     print db.command("collstats", collection, scale=1024)
 
 
-@scheduler.interval_schedule(minutes=15)
+@scheduler.cron_schedule(minute='0/5')
 def send_statistics_job():
     for collection in find_all_collections():
         send_collection_stats(collection)
 
 
 scheduler.start()
+atexit.register(lambda: scheduler.shutdown(wait=False))
 while True:
     pass
