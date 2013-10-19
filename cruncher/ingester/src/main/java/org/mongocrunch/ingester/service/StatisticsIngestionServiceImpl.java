f2e95e44e813775bec5b80910b26e3055127e21d
mzasada <maciejekz@gmail.com>
2013-10-20 01:45:22 +0200

    Added task executor for @Async support.

--- cruncher/ingester/src/main/java/org/mongocrunch/ingester/service/StatisticsIngestionServiceImpl.java
+++ cruncher/ingester/src/main/java/org/mongocrunch/ingester/service/StatisticsIngestionServiceImpl.java
@@ -2,13 +2,19 @@ package org.mongocrunch.ingester.service;
 
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StatisticsIngestionServiceImpl implements StatisticsIngestionService {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(StatisticsIngestionServiceImpl.class);
+
   @Async
   @Override
   public void saveCollectionStatistics(String clientUID, Map<String, Object> statistics) {
+    LOGGER.info("Storing collection statistics for client {}", clientUID);
   }
 }
